1a24b53d5eb93e3ab754f0181ed9e69edd830a54
mrifainestli7 <101726009+mrifainestli7@users.noreply.github.com>
2022-07-21 17:58:13 +0700

    Update lihat.php
    Responsi PWeb

--- lihat.php
+++ lihat.php
@@ -1,21 +1,39 @@
+<head>
+    <title>Data Customer</title>
+    <style>
+        .x{
+            padding: 10px;
+        }
+        body {
+            margin:0;
+            padding:0;
+            font-family: sans-serif;
+            background: linear-gradient(#F78812, #925611);
+        }
+    </style>
+</head>
+<body>
+    <div>
+        <a href="login.html"><img class="x" src="panah.png"></a>
+    </div>
+</body>
 <?php
 $user = $_POST['user'];
 $pass = $_POST['pw'];
 if($user=="Admin" && $pass=="Admin"){
-    echo "<head><title>Data Customer</title> </head>"; 
     $fp = fopen("file.txt","r");
     $tanggal=date("l, j F Y");
-    echo "<body bgcolor=#c4a27c><center>";
-    echo "<br><h1>Data Customer</h1><br>";
-    echo "|<b>Tanggal :</b> $tanggal|";
-    echo "<br><br><a href=index.html>::Beranda::</a>";
+    echo "<center><br><h1>Data Customer</h1><br>";
+    echo "|<b>Tanggal :</b> $tanggal|<br><br>";
     echo "<hr color=#3c2800>";
     echo "<table border=1 width=85% bgcolor=#FAEEE0>";
     echo "<tr bgcolor=#e7fae0><td> Tanggal </td>";
     echo "<td> Jam </td>";
     echo "<td> Nama </td>";
     echo "<td> Layanan </td>";
-    echo "<td> Email </td></tr>";
+    echo "<td> Total </td>";
+    echo "<td> Email </td>";
+    echo "<td> No HP </td></tr>";
     while ($isi = fgets($fp, 900))
     {
         $pisah = explode("|", $isi);
@@ -23,7 +41,9 @@ if($user=="Admin" && $pass=="Admin"){
         echo "<td>" .$pisah[1]. "</td>";
         echo "<td>" .$pisah[2]. "</td>";
         echo "<td>" .$pisah[3]. "</td>";
-        echo "<td>" .$pisah[4]. "</td></tr>";
+        echo "<td>" .$pisah[4]. "</td>";
+        echo "<td>" .$pisah[5]. "</td>";
+        echo "<td>" .$pisah[6]. "</td></tr>";
     }
     echo "</table>";
     echo "</body></center>";
