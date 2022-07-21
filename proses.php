f1043ecbebc6f282b1f0519a1eaa8571ac1a62b2
mrifainestli7 <101726009+mrifainestli7@users.noreply.github.com>
2022-07-21 18:00:02 +0700

    Update proses.php
    Responsi PWeb

--- proses.php
+++ proses.php
@@ -36,27 +36,35 @@
             $tanggal = $_POST['tanggal'];
             $jam = $_POST['jam'];
             $email = $_POST['email'];
+            $nomor = $_POST['nohp'];
+            $total=0;
             $pesan=" ";
             if(isset($_POST['pil1'])){
                 $pesan.="Cukur Rambut ,";
+                $total+=100000;
             }
             if(isset($_POST['pil2'])){
                 $pesan.="Tata Jenggot/Kumis ,";
+                $total+=80000;
             }
             if(isset($_POST['pil3'])){
                 $pesan.="Creambath ,";
+                $total+=60000;
             }
             if(isset($_POST['pil4'])){
                 $pesan.="Pijat Reflexi ,";
+                $total+=50000;
             }
             if(isset($_POST['pil5'])){
                 $pesan.="Pewarnaan Rambut ,";
+                $total+=110000;
             }
             if(isset($_POST['pil6'])){
                 $pesan.="Smoothing ,";
+                $total+=80000;
             }
             $fp = fopen("file.txt", "a+"); 
-            fputs($fp, "$tanggal | $jam | $nama | $pesan | $email \n"); 
+            fputs($fp, "$tanggal | $jam | $nama | $pesan | $total | $email | $nomor\n"); 
             fclose($fp);
         ?>
     </body>
